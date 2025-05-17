149ebdfa7599a2c586d443824bd0188f33881d2b
Eric Alfred <alfrederic371@gmail.com>
2025-05-17 03:02:24 +0100

    Add movie search functionality and implement custom SearchBar component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,45 @@
+import SearchBar from "@/components/SearchBar"
 import { icons } from "@/constants/icons"
 import { images } from "@/constants/images"
-import { Image, ScrollView, View } from "react-native"
+import { fetchMovies } from "@/services/api"
+import useFetch from "@/services/usefetch"
+import { useRouter } from "expo-router"
+import React from "react"
+import { ActivityIndicator, Image, ScrollView, Text, View } from "react-native"
 
 export default function Index() {
+  const router = useRouter()
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() => fetchMovies({ query: "" }));
+
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute top-0 left-0 right-0 bottom-0 w-full z-0" />
 
       <ScrollView className="flex-1 px-5" showsHorizontalScrollIndicator={false} showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingBottom: 10, minHeight: "100%" }}>
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+
+      {moviesLoading ? (
+        <ActivityIndicator
+          color={"#000ff"}
+          className="mt-10 self-center"
+          size="large"
+        />
+      ) : moviesError ? (
+        <Text>Error: {moviesError?.message}</Text>
+      ) : (
+        <View className="flex-1 mt-5">
+          <SearchBar onPress={() => router.push("/search")} placeholder="Search for a movie..." />
+
+            <>
+              <Text className="text-lg text-white font-bold mt-5 mb-3">Trending Movies</Text>
+            </>
+        </View>
+      )}
       </ScrollView>
     </View>
   )
